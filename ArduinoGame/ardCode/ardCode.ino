b8601f295f8734aa09532057f5892c3e7411259d
Dalton F <83785366+daltonfern@users.noreply.github.com>
2021-05-17 18:07:35 -0400

    Add files via upload

---
+++ ArduinoGame/ardCode/ardCode.ino
@@ -0,0 +1,17 @@
+int FA = A0;
+int FD = 8;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(FA, INPUT);
+pinMode(FD, INPUT);
+digitalWrite(FD, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int fireState = digitalRead(FD);
+if ( fireState == HIGH) {
+  Serial.println("Fire is on");
+}
+}
